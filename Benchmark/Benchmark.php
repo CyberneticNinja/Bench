02a6ee289158d5c0edac8fac202dacbb46604bd2
abrokwahs <AbrokwahS@gmail.com>
2018-11-08 06:54:00 -0500

    -Benchmark class logic

--- Benchmark/Benchmark.php
+++ Benchmark/Benchmark.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * User: abrokwahs
- * Date: 11/7/18
- * Time: 5:03 PM
+ * Date: 11/6/18
+ * Time: 11:41 AM
  */
 
 namespace Benchmark;
@@ -11,7 +11,40 @@ namespace Benchmark;
 
 class Benchmark
 {
-    public static function helloWorld(){
-        return "Hello World";
+
+    /**
+     * Benchmark constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    //storedResults = stores functions
+    protected $storedResults = [];
+
+    //getMicroTime() returns timestamp in microseconds
+    protected function getMicroTime()  {
+        return microtime(true);
+    }
+
+    //runBenchmark(callable $func,$runNumber,array $parameters),
+    public function runBenchmark(callable $func,$runNumber,array $parameters) {
+
+        $resultSet = [];
+        for($count = 0;$count <$runNumber;$count++) {
+            $start = $this->getMicroTime();
+            call_user_func_array($func,$parameters);
+            $end = $this->getMicroTime();
+
+            $roundedResult = round($end-$start,8);
+            $resultSet[] = $roundedResult;
+        }
+        $this->storedResults[(string)$func ] = $resultSet;
+//        dump($this->storedResults);
+    }
+
+    //returns all the benchmarks
+    public function getstoredResults()  {
+        return $this->storedResults;
     }
 }
--- index.php
+++ index.php
@@ -6,10 +6,31 @@
  * Time: 4:07 PM
  */
     require('vendor/autoload.php');
-    use Benchmark\Benchmark as bench;
+    use Benchmark\Benchmark;
 
     $whoops = new \Whoops\Run;
     $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
     $whoops->register();
 
-    echo bench::helloWorld();
+    function hi(){
+        sleep(5);
+        echo 'hello world'."<br/>";
+    }
+
+    function tired(){
+        echo ' I am tired let me sleep a bit <br/>';
+        sleep(6);
+        echo ' I am awake now <br/>';
+    }
+
+    function favorite($movie){
+        sleep(7);
+        echo "My favorite movie is $movie ".'<br/>';
+    }
+
+    $bench = new Benchmark();
+
+    $bench->runBenchmark("tired",3,[]);
+    $bench->runBenchmark("favorite",3,["The Godfather:Part II"]);
+    $bench->runBenchmark("hi" ,5,[]);
+
